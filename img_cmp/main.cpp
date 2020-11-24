5ea216fcd980c93e23d090cb362bbf34c6d2953a
kdimtri <kurunov.dmitry@icloud.com>
2020-11-24 14:25:04 +0400

    Initial Commit

---
+++ img_cmp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  img_cmp
+//
+//  Created by Allan Hein on 24.11.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
